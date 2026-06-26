039a2caa8d865d22a232e43869d64210d29c53de
kamakitchefao160-eng <kamakit.chefao160@gmail.com>
2026-06-26 10:53:44 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 export const PREFIX = "/";
 
 // Emoji do bot (mude se preferir).
-export const BOT_EMOJI = "🤖";
+export const BOT_EMOJI = "🌸";
 
 // Nome do bot (mude se preferir).
 export const BOT_NAME = "Takeshi Bot";
