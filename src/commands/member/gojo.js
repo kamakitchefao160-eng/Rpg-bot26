9406e463682eff27dbc88057a1e7df9def917b3f
kamakitchefao160-eng <kamakit.chefao160@gmail.com>
2026-07-16 14:49:12 -0300

    Update gojo.js

--- src/commands/member/gojo.js
+++ src/commands/member/gojo.js
@@ -43,7 +43,7 @@ export default {
           { role: "system", content: contextoGeral },
           { role: "user", content: mensagemUsuario }
         ],
-        model: "llama3-8b-8192",
+        model: "llama-3.3-70b-versatile",
       });
 
       const resposta = chatCompletion.choices[0]?.message?.content || "Relaxa, afinal, eu sou o mais forte. 😉";
