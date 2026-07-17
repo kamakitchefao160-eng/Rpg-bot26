e62b2609419b7dfefb4c27e774d7837bed33f14f
kamakitchefao160-eng <kamakit.chefao160@gmail.com>
2026-07-17 10:41:38 -0300

    Update raiden.js

--- src/commands/member/raiden.js
+++ src/commands/member/raiden.js
@@ -52,7 +52,7 @@ export default {
           { role: "system", content: `${contextoGeral}\n${contextoPersonalizado}` },
           { role: "user", content: mensagemUsuario }
         ],
-        model: "model: "llama-3.1-8b-instant",
+        model: "llama-3.1-8b-instant",
       });
 
       const resposta = chatCompletion.choices[0]?.message?.content || "Estou sem tempo para bobagens.";
